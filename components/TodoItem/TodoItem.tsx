c377fb19c104eb19a4d73cf2536def4b022351b2
ikey2244 <ikey2244@gmail.com>
2022-02-05 14:25:55 -0600

    Add updates

--- components/TodoItem/TodoItem.tsx
+++ components/TodoItem/TodoItem.tsx
@@ -12,13 +12,13 @@ const TodoItem = ({ todo, toggleComplete, deleteTodo, updateTodo }: TodoItemProp
   return (
     <div className='flex relative rounded-sm p-4 shadow-sm justify-between bg-gray-100'>
       <div className="flex space-x-4 items-center">
-        <p className={todo.completed ? 'line-through' : ""}>{todo.title}</p>
+        <p className={todo?.completed ? 'line-through' : ""}>{todo?.title}</p>
         <div className="bg-green-200 rounded-md p-2">
-          <p>{todo.category}</p>
+          <p>{todo?.category}</p>
         </div>
       </div>
 
-      <TodoActions todoId={todo.id} toggleComplete={toggleComplete} deleteTodo={deleteTodo} updateTodo={updateTodo} />
+      <TodoActions todoId={todo?.id} toggleComplete={toggleComplete} deleteTodo={deleteTodo} updateTodo={updateTodo} />
     </div>
   )
 }
